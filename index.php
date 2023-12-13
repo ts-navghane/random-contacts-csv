709a8b60331f4417a78f8314549a570148dc32d9
Tejas Navghane <ts.navghane@gmail.com>
2023-12-13 14:17:08 +0530

    Reset CSV columns to bare minimim

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ $csvFilename = $numContacts.'_contacts.csv';
 $csvFile = fopen($csvFilename, 'wb');
 
 // Add a header row to the CSV file
-fputcsv($csvFile, ['firstname', 'lastname', 'email', 'phone', 'address1', 'address2', 'address3', 'sentence']);
+fputcsv($csvFile, ['firstname', 'lastname', 'email']);
 
 $start = microtime(true);
 
@@ -31,18 +31,13 @@ for ($i = 0; $i < $numContacts; $i++) {
     $firstname = $faker->firstName;
     $lastname  = $faker->lastName;
     $email     = strtolower($firstname.'.'.$lastname.'@mailtest.mautic.com');
-    $phone     = $faker->phoneNumber;
-    $address1  = $faker->address;
-    $address2  = $faker->address;
-    $address3  = $faker->address;
-    $sentence  = $faker->sentence;
-    fputcsv($csvFile, [$firstname, $lastname, $email, $phone, $address1, $address2, $address3, $sentence]);
+    fputcsv($csvFile, [$firstname, $lastname, $email]);
 }
 
 // Close the CSV file
 fclose($csvFile);
 
-$end = microtime(true);
+$end           = microtime(true);
 $executionTime = number_format($end - $start, 4);
 
 echo "CSV file with $numContacts records has been created: $csvFilename in $executionTime seconds\n";
